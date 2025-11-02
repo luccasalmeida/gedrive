f1f2ed7d3821fd387c208a1d044073c3ac68725c
Lucas de Almeida <developer.lucas24@gmail.com>
2025-11-02 23:21:08 +0100

    feat: changes tomcat to undertow server

--- src/main/java/com/ldsa/gedrive/controllers/GoogleDriveFileController.java
+++ src/main/java/com/ldsa/gedrive/controllers/GoogleDriveFileController.java
@@ -3,6 +3,7 @@ package com.ldsa.gedrive.controllers;
 import com.ldsa.gedrive.dtos.GoogleDriveFileDTO;
 import com.ldsa.gedrive.services.GoogleDriveFileService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -34,20 +34,20 @@ public class GoogleDriveFileController {
 
     @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public String upload(
-            @RequestParam("file") MultipartFile file,
+            @RequestPart("file") MultipartFile file,
             @RequestParam("path") String path,
             @RequestParam("shared") String shared) {
         path = "".equals(path) ?"Root" : path;
         return googleDriveFileService.upload(file, path, Boolean.parseBoolean(shared));
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}/delete")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String id) {
         googleDriveFileService.deleteById(id);
     }
 
-    @GetMapping("/download/{id}")
+    @GetMapping("/{id}/download")
     @ResponseStatus(HttpStatus.OK)
     public void download(@PathVariable String id, HttpServletResponse response) throws IOException {
         googleDriveFileService.download(id, response.getOutputStream());
